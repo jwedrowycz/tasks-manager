1a05592bbc2b7eeb4202a9e451bd232212e0459e
Jakub Kopniewski <jkopniewski@womp.local>
2021-07-20 14:36:38 +0200

    added tasks table

---
+++ database/migrations/2021_07_20_121251_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->datetime('start');
+            $table->datetime('end');
+            $table->boolean('is_private');
+            $table->datetime('expected_end');
+            $table->boolean('is_active');
+            $table->foreignId('registered_by')->constrained('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
